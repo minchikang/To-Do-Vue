8d0fe524d295ec85e4462ac01e84754b9be9d1dc
grieta.mj.kang <kangminchi@gangminjiui-MacBookAir.local>
2021-06-29 09:08:30 +0900

    home 수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>MY TODO LIST</h1>
+    <ul>
+      <li>스터디 공부하기</li>
+      <li>냉면 사오기</li>
+      <li>업무</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style scoped lang="scss">
+.home{
+  background: #ECF0F3;
+  padding: 50px;
+}
+h1 {
+  margin: 30px auto;
+  width: 300px;
+  height: 60px;
+  line-height: 60px;
+  color: #555;
+  background: #E3E6EC;
+  box-shadow: 5px 5px 10px #D1D9E6, -5px -5px 10px #FFFFFF;
+  border-radius: 10px;
+}
+ul{
+  margin: 0 auto;
+  width: 360px;
+  height: 450px;
+  background: #E3E6EC;
+  box-shadow: 5px 5px 10px #D1D9E6, -5px -5px 10px #FFFFFF;
+  border-radius: 10px;
+  padding: 30px 30px;
+  li {
+    list-style: none;
+    width: calc(100%-10px);
+    height: 40px;
+    line-height: 40px;
+    text-align: left;
+    background: #E6E9EF;
+    box-shadow: inset 2px 2px 10px #D1D9E6, inset -2px -2px 10px #FFFFFF;
+    border-radius: 8px;
+    padding: 5px 10px;
+    margin-bottom: 20px;
+    color: #3F4A62;
+
+  }
+}
+</style>
